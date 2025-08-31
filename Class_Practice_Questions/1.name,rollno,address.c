06f035714796f956399d02ea016fe39d3097fe4b
Prabhjot Singh <amssre.16461@gmail.com>
2025-09-01 02:17:25 +0530

    Question23and24

--- Class_Practice_Questions/1.name,rollno,address.c
+++ Class_Practice_Questions/1.name,rollno,address.c
@@ -3,8 +3,19 @@
 
 int main()
 {
-    printf("My name is Prabhjot Singh");
-    printf("roll no =5900xxxxx");
-    printf("address= F-13,Keshav nagar numaish camp");
+    char name[20];
+    int roll_no;
+    char address[20];
+    printf("Enter the student name");
+    scanf("%s",&name);
+    fflush(stdin);
+    printf("Enter the student roll no=");
+    scanf("%d",&roll_no);
+    fflush(stdin);
+    printf("Enter the address");
+    gets(address);
+    printf("student name=%s\n",name);
+    printf("roll no =%d\n",roll_no);
+    printf("address= %s\n",address);
     return 0;
 }
