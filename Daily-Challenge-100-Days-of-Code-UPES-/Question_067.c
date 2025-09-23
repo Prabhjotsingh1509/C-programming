135276b5c8ac41185890f38aaf30f730c9155e8e
Prabhjot Singh <amssre.16461@gmail.com>
2025-09-24 01:06:05 +0530

    67and68

---
+++ Daily-Challenge-100-Days-of-Code-UPES-/Question_067.c
@@ -0,0 +1,47 @@
+// Q67: Insert an element in an array at a given position.
+#include<stdio.h>
+int insert(int a[],int pos,int num,int n);
+int main()
+{
+    int n,a[10],i,pos,num;
+    printf("Size of array:");
+    scanf("%d",&n);
+    printf("Enter the elements of array");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    printf("Enter the position:");
+    scanf("%d",&pos);
+    printf("Enter the number you want to insert:");
+    scanf("%d",&num);
+    insert(a,pos,num,n);
+    return 0;
+}
+int insert(int a[],int pos,int num,int n)
+{   int i;
+    for(i = n; i > pos; i--)
+    {
+        a[i] = a[i - 1];
+    }
+    a[pos] = num;
+
+    printf("The new array:");
+    for(i = 0; i <= n; i++)
+    {
+        printf("%d\t", a[i]);
+    }
+
+    return 0;
+
+}
+/*
+Sample Test Cases:
+Input 1:
+4
+10 20 30 40
+2 15
+Output 1:
+10 20 15 30 40
+
+*/
