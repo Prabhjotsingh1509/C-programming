896ab52dc870373499b13a285bb874eb617ccaad
Prabhjot Singh <amssre.16461@gmail.com>
2025-09-23 23:34:38 +0530

    65and66

---
+++ Daily-Challenge-100-Days-of-Code-UPES-/Question_065.c
@@ -0,0 +1,66 @@
+// Q65: Search in a sorted array using binary search.
+#include<stdio.h>
+int binarySearch(int arr[],int size,int target);
+int main()
+{
+        int arr[10],n,i,result,target;
+        int size=sizeof(arr)/sizeof(arr[0]);
+        printf("Enter the size of array:");
+        scanf("%d",&n);
+        printf("Enter the elements of array:");
+        for(i=0;i<n;i++)
+        {
+            scanf("%d",&arr[i]);
+        }
+        printf("Enter the element to be searched:");
+        scanf("%d",&target);
+        result=binarySearch(arr,size,target);
+        if(result==-1)
+        {
+            printf("Element not found");
+        }
+        else
+        {
+            printf("Element found at index %d",result);
+        }
+        return 0;
+}
+int binarySearch(int arr[],int size,int target)
+{
+    int left=0,right=size-1,mid;
+    while(left<=right)
+    {
+        mid=left+(right-left)/2;
+        if(arr[mid]==target)
+        {
+            return mid;
+        }
+
+        if(arr[mid]>target)
+        {
+            right=mid-1;
+        }
+        else
+        {
+            left=mid+1;
+        }
+    }
+        return -1;      
+}
+/*
+Sample Test Cases:
+Input 1:
+5
+1 3 5 7 9
+7
+Output 1:
+Found at index 3
+
+Input 2:
+5
+1 3 5 7 9
+6
+Output 2:
+-1
+
+*/
