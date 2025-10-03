1fb04b4722b113c69087a1dead36b3a865d2c27e
Prabhjot Singh <amssre.16461@gmail.com>
2025-10-04 01:22:18 +0530

    Q85-86

--- Daily-Challenge-100-Days-of-Code-UPES-/Question_082.c
+++ Daily-Challenge-100-Days-of-Code-UPES-/Question_082.c
@@ -6,7 +6,7 @@ int main()
     int i=0;
     char str[100];
     printf("Enter the string:");
-    scanf("%s",&str);
+    scanf("%s",str);
 
     while(str[i]!= '\0')
     {
--- Daily-Challenge-100-Days-of-Code-UPES-/Question_083.c
+++ Daily-Challenge-100-Days-of-Code-UPES-/Question_083.c
@@ -6,7 +6,7 @@ int main()
     int i=0,v=0,c=0;
     char str[10];
     printf("Enter the string:");
-    scanf("%s",&str);
+    scanf("%s",str);
     
     while(str[i] != '\0')
     {
