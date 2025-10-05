3c6626bff92c3adfff8bb1dd8947ee778a3ab679
Prabhjot Singh <amssre.16461@gmail.com>
2025-10-05 17:35:31 +0530

    Q91-92

--- Daily-Challenge-100-Days-of-Code-UPES-/Question_092.c
+++ Daily-Challenge-100-Days-of-Code-UPES-/Question_092.c
@@ -3,20 +3,20 @@
 
 int main()
 {
-    int i=0,l=0,c;
+    int i=0,len=0,c;
     char str[50];
     printf("Enter the string:");
     scanf("%s",str);
     while(str[i]!='\0')
     {
-        l++;
+        len++;
         i++;
     }
     i=0;
     while(str[i] != '\0')
     {
         c=i+1;
-        while(c<l && str[i]>=97 && str[i]<=122)
+        while(c<len && str[i]>=97 && str[i]<=122)
         {
             if(str[i]==str[c])
             {
@@ -27,6 +27,7 @@ int main()
         }
         i++;
     }
+    printf("No repeating lowercase character found.\n");
     return 0;
 }
 /*
